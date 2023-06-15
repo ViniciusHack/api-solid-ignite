2875e1bbeea04dafe6fa550be2b8554b6dcd4d0c
vinicius <viniciusvwh1441@gmail.com>
2023-06-15 00:18:54 -0300

    feat: validate check-in

--- src/repositories/check-ins-repository.ts
+++ src/repositories/check-ins-repository.ts
@@ -4,6 +4,8 @@ import { CheckIn, Prisma } from ".prisma/client";
 export interface CheckInsRepository {
   findByUserIdOnDate(userId: string, date: Date): Promise<CheckIn | null>
   findManyByUserId(userId: string, page: number): Promise<CheckIn[]>
+  findById(checkInId: string): Promise<CheckIn | null>
   countByUserId(userId: string): Promise<number>;
+  save(checkIn: CheckIn): Promise<CheckIn>
   create(data: Prisma.CheckInUncheckedCreateInput): Promise<CheckIn>
 }
--- src/repositories/in-memory/in-memory-gyms-repository.ts
+++ src/repositories/in-memory/in-memory-gyms-repository.ts
@@ -1,11 +1,9 @@
+import { getDistanceBetweenCoordinates } from "@/utils/get-disctance-between-coordinates";
 import { Gym, Prisma } from "@prisma/client";
 import { randomUUID } from "crypto";
-import { GymsRepository } from "../gyms-repository";
+import { FindManyNearbyParams, GymsRepository } from "../gyms-repository";
 
-export class InMemoryGymsRepository implements GymsRepository {
-  async searchMany(query: string, page: number) {
-    return this.items.filter(item => item.title.includes(query)).slice((page -1) * 20, page * 20)
-  }
+export class InMemoryGymsRepository implements GymsRepository {  
   public items: Gym[] = [];
 
   async findById(id: string) {
@@ -16,6 +14,24 @@ export class InMemoryGymsRepository implements GymsRepository {
     }
 
     return gym
+  }
+
+  async searchMany(query: string, page: number) {
+    return this.items.filter(item => item.title.includes(query)).slice((page -1) * 20, page * 20)
+  }
+
+  async findManyNearby(params: FindManyNearbyParams) {
+    return this.items.filter(item => {
+      const distance = getDistanceBetweenCoordinates({
+        latitude: params.latitude,
+        longitude: params.longitude
+      }, {
+        latitude: item.latitude.toNumber(),
+        longitude: item.longitude.toNumber(),
+      })
+
+      return distance < 10
+    })
   }
 
   async create({ id, title, description, phone, latitude, longitude}: Prisma.GymUncheckedCreateInput) {
